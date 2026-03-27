91fc6c59e706df108d2e071597a5e02675dea427
William Strandqvist <strandqvist.william@gmail.com>
2026-03-27 11:38:52 +0100

    feat: Update game card layout and enhance scrollbar styling for improved UI

--- components/game_card.php
+++ components/game_card.php
@@ -13,4 +13,25 @@ if (!isset($game_data)) {
     <div class="top">
         <img src="<?php echo $game_data["background_image"]; ?>" alt="">
     </div>
+    <div class="bottom">
+        <div class="title">
+            <?php echo $game_data["name"]; ?>
+        </div>
+        <div class="plattforms">
+            <?php foreach ($game_data["parent_platforms"] as $platform) {
+                ?> 
+                
+                <?php
+                echo $platform["platform"]["slug"];
+            } ?>
+        </div>
+        <div class="icon-info">
+            <div class="rating">
+                <?php echo $game_data["rating"] ?>
+            </div>
+            <div class="comments">
+
+            </div>
+        </div>
+    </div>
 </div>
